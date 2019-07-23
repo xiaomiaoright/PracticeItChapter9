de7fc0f8ab1abf6db8d728c26b23a3d55d1be154
Mengmeng Miao <32528587+xiaomiaoright@users.noreply.github.com>
2019-07-23 14:32:55 -0600

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
